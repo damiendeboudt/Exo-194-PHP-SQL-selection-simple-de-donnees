6494198705f52fa186eb5b247b32c146c75efbbb
Damien <damien.deboudt@sfr.fr>
2023-02-13 13:37:57 +0100

    ok

--- index.php
+++ index.php
@@ -1,5 +1,54 @@
 <?php
 
+try {
+    $server = 'localhost';
+    $db = 'exo_194';
+    $user = 'root';
+    $password = '';
+
+    $bdd= new PDO("mysql:host=$server;dbname=$db; charset=utf8", $user, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $stmt = $bdd->prepare("SELECT nom, prenom, rue, numero, code_postal, ville, pays, mail from user");
+
+    $state = $stmt->execute();
+
+    if ($state) {
+       foreach ($stmt->fetchAll() as $user) {
+           echo "Utilisateur: " . $user['nom']. ' ' . $user['prenom'] . ' ' . $user['rue'] . ' ' . $user['numero'] .
+               ' ' . $user['code_postal'] . ' ' . $user['ville'] . ' ' . $user['pays'] . ' ' . $user['mail'] . "<br>";
+       }
+    } else {
+        echo 'erreur';
+    }
+
+    $stmt2 = $bdd->prepare("SELECT * from user ORDER BY id DESC");
+
+    $state2 = $stmt2->execute();
+
+    if ($state2) {
+        foreach ($stmt2->fetchAll() as $user) {
+            echo "Utilisateur: " . $user['id'] . $user['nom']. ' ' . $user['prenom'] . ' ' . $user['rue'] . ' ' . $user['numero'] .
+                ' ' . $user['code_postal'] . ' ' . $user['ville'] . ' ' . $user['pays'] . ' ' . $user['mail'] . "<br>";
+        }
+    } else {
+        echo 'erreur';
+    }
+
+    $stmt3 = $bdd->prepare("SELECT id, nom, prenom from user ORDER BY id DESC");
+
+    $state3 = $stmt3->execute();
+
+    if($state3) {
+        foreach ($stmt3->fetchAll() as $user) {
+            echo "User: " . $user["id"] . ' ' .  $user['nom'] . ' ' . $user['prenom'] . "<br>";
+        }
+    }
+}
+catch(PDOException $exceptions) {
+    echo $exceptions->getMessage();
+}
 /**
  * 1. Importez le fichier SQL se trouvant dans le dossier SQL.
  * 2. Connectez vous à votre base de données avec PHP
